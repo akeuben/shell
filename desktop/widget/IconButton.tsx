55bf7a33d42dcd2f77abc6ec24ab97d2358af34f
Avery Keuben <avery1516@gmail.com>
2025-09-24 18:13:28 -0600

    Add network settings

--- desktop/widget/IconButton.tsx
+++ desktop/widget/IconButton.tsx
@@ -1,8 +1,9 @@
 import { Accessor } from "gnim"
 import { pointer } from "../util/cursor"
+import { Gtk } from "ags/gtk4"
 
-export const IconButton = ({onClicked, icon_name, pixel_size, className, hexpand, vexpand}: {onClicked: () => void, icon_name: string, pixel_size: number, className: string | Accessor<string> | undefined, hexpand?: boolean | Accessor<boolean>, vexpand?: boolean |Accessor<boolean>}) => {
-    return <button hexpand={hexpand} vexpand={vexpand} onClicked={onClicked} cursor={pointer} class={["undefined", "string"].includes(typeof className) ? `${className} icon-button` : (className as Accessor<String>).as(c => `${c} icon-button`)} >
+export const IconButton = ({onClicked, icon_name, pixel_size, className, hexpand, vexpand, halign, valign}: {onClicked: () => void, icon_name: string, pixel_size: number, className: string | Accessor<string> | undefined, hexpand?: boolean | Accessor<boolean>, vexpand?: boolean |Accessor<boolean>, halign?: Gtk.Align | Accessor<Gtk.Align>, valign?: Gtk.Align | Accessor<Gtk.Align>}) => {
+    return <button hexpand={hexpand} vexpand={vexpand} onClicked={onClicked} cursor={pointer} class={["undefined", "string"].includes(typeof className) ? `${className} icon-button` : (className as Accessor<String>).as(c => `${c} icon-button`)} halign={halign} valign={valign} >
         <image icon_name={icon_name} pixel_size={pixel_size}/>
     </button>
 }
